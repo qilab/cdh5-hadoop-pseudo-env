06f8dc41c12603b69d51d027546f71220ad9e113
qilab- <qilab@gmx.com>
2016-03-24 01:19:38 +0900

    CircleCI test

---
+++ test/Ubuntu_14.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ANSIBLE_PLAYBOOK=/test_ansible/ansible/site.yml
+ANSIBLE_HOSTS=/test_ansible/ansible/hosts
+ANSIBLE_HOSTS_TMP=/tmp/ansible_hosts
+
+if [ ! -f $ANSIBLE_PLAYBOOK ]; then
+  echo "Cannot find Ansible playbook"
+  exit 1
+fi
+
+if [ ! -f $ANSIBLE_HOSTS ]; then
+  echo "Cannot find Ansible hosts"
+  exit 2
+fi
+
+apt-get -y install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get -y update
+apt-get -y install ansible
+
+cp $ANSIBLE_HOSTS $ANSIBLE_HOSTS_TMP
+chmod -x $ANSIBLE_HOSTS_TMP
+echo "Running Ansible"
+ansible-playbook -i $ANSIBLE_HOSTS_TMP $ANSIBLE_PLAYBOOK --extra-vars "test=yes" -v --connection=local
